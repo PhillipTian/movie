c0384bb36d92813ac5aedff89dc6ea43d5c1a964
PhillipTian <87056960+PhillipTian@users.noreply.github.com>
2022-07-10 15:16:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome,dear passenger</h1>
+      <h2>Watch right now!</h2>
       <VideoPlayer
         url="https://ktpublic-pics.s3.amazonaws.com/SampleVideo_720x480_2mb.mp4"
         autosize
